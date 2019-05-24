f534246aa9b621622c48a3b6180888ad7361a321
Dmytro Panontko <dmytro.panontko@keenethics.com>
2019-05-24 15:54:20 +0300

    Added docs and change modal width.

--- src/openHelp.js
+++ src/openHelp.js
@@ -1,5 +1,5 @@
 function openHelp() {
-  var html = HtmlService.createHtmlOutputFromFile('help');
+  var html = HtmlService.createHtmlOutputFromFile('help').setWidth(800);
   SpreadsheetApp.getUi()
       .showModalDialog(html, t('Help'));
 }
